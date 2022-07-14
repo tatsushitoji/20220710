37ddb299910c60b0f878de9d019074d5b446c676
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2022-07-15 00:45:55 +0900

    chore: customize eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,23 @@
+/** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "next",
+    "next/core-web-vitals",
+    "prettier",
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "react/function-component-definition": [
+      "error",
+      { namedComponents: "arrow-function" },
+    ],
   },
 };
