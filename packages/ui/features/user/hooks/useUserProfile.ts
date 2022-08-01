8510833dc98dc77e2842ab4c16c98e5150facf8d
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2022-08-01 11:13:45 +0900

    feat: implement skeleton and exception

--- packages/ui/features/user/hooks/useUserProfile.ts
+++ packages/ui/features/user/hooks/useUserProfile.ts
@@ -2,14 +2,15 @@ import { api } from "graphql-codegen";
 import { User } from "../types";
 
 export const useUserProfile = () => {
-  const [{ data, fetching }] = api.useUserProfileQuery();
-  const user: User = {
-    avatarUrl: data?.viewer.avatarUrl as string,
-    name: data?.viewer.login || "",
-    resourcePath: data?.viewer.resourcePath as string,
-  };
+  const [{ data }] = api.useUserProfileQuery();
+  const user: User | undefined = data
+    ? {
+        avatarUrl: data?.viewer.avatarUrl,
+        name: data?.viewer.login,
+        resourcePath: data?.viewer.resourcePath,
+      }
+    : undefined;
   return {
-    userData: user,
-    loading: fetching,
+    user,
   };
 };
