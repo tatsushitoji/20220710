6dd2ea502f6ba5b2e07afb08cdb1292bcaaf3f26
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2022-08-05 18:42:50 +0900

    chore: lint is unnecessary at storybook package

--- packages/storybook/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
-
-module.exports = {
-  root: true,
-  extends: ['custom'],
-  parserOptions: {
-    project: ['./tsconfig.json'],
-  },
-}
